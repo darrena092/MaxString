1bc1ed2bc7e7e6c98f310546ab54af70740d85ad
Darren Anderson <darren@darrenanderson.dev>
2022-11-27 10:45:22 +0000

    Copyright notices and include guards.

--- include/max-string.h
+++ include/max-string.h
@@ -1,4 +1,29 @@
-#pragma once
+/*
+    MIT License
+
+    Copyright (c) 2022 Darren Anderson
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+#ifndef MAX_STRING_H
+#define MAX_STRING_H
 
 #include <string.h>
 
@@ -8,3 +33,5 @@ char* mstrsubstr(const char *input, int offset, size_t length);
 char* mstrreplace(const char *input, char find, char replace);
 char* mstrremove(const char *input, char garbage);
 char** mstrsplit(const char *input, char delimiter, int* token_count);
+
+#endif
